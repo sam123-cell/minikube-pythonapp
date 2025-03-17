6eed41df181ded248f1bffa4084f4aa04dfc72f1
sam123-cell <amitsinghc9@gmail.com>
2025-03-17 08:42:36 +0000

    chore: pushed minikube script as well

---
+++ scripts/minikube.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "This setup is to deploy minikube cluster only"
+
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+minikube start
